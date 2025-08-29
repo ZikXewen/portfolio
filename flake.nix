b4c8445b281a0b2606a58dc2548a1d56ff3ab016
ZikXewen <datasup12@gmail.com>
2025-08-29 13:46:33 -0500

    add flake

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "Flake for development";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    pkgs = nixpkgs.legacyPackages."x86_64-linux";
+  in {
+    devShells."x86_64-linux".default = pkgs.mkShell {
+      packages = [pkgs.nodejs pkgs.pnpm];
+    };
+  };
+}
