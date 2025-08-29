d922f722330af9140b8e8bbc1bbab58b0be9cfae
ZikXewen <datasup12@gmail.com>
2025-08-29 14:17:10 -0500

    move stuffs to new site

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,9 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [],
-
+  redirects: {
+    "/": "https://zikxewen.github.io"
+  },
   vite: {
     plugins: [tailwindcss()],
   },
